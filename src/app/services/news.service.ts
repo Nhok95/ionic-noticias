d52d0bbbbb7f3a718ad63bb5b34e3f93489dd5e1
Nhok95 <enrique_1095@hotmail.es>
2021-01-16 18:05:58 +0100

    Updated news.service.ts, added type

--- src/app/services/news.service.ts
+++ src/app/services/news.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { ResponseTopHeadlines } from '../interfaces/interfaces';
 
 @Injectable({
   providedIn: 'root'
@@ -9,6 +10,6 @@ export class NewsService {
   constructor( private http: HttpClient ) { }
 
   getTopHeadLines() {
-    return this.http.get('http://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=fba5c5d7552e4966882ea5b679f92ae9');
+    return this.http.get<ResponseTopHeadlines>('http://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=fba5c5d7552e4966882ea5b679f92ae9');
   }
 }
